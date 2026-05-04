28d0ad4a13f43aeb59b723e71ec98aff14ad2659
gcolquefl <gcolquefl@unsa.edu.pe>
2026-05-03 23:10:05 -0500

    Agregue función de impresión y añadí un if en multiplyStandar para la validación de tamaños en matrices

--- main.cpp
+++ main.cpp
@@ -16,7 +16,14 @@ Matriz generateMatriz(int n) {
   return newMatriz;
 }
 
-Matriz multiplyStandar(const Matriz& a, const Matriz& b, int n) {
+Matriz multiplyStandar(const Matriz& a, const Matriz& b) {
+  if(a.size() == 0 || b.size() == 0 || a.size() != b.size() ||
+      a[0].size() != b[0].size() || a.size() != a[0].size()) {
+    cout << "Dimensiones invalidas" << endl;
+    return Matriz();
+  }
+
+  int n = a.size();
   Matriz c(n, vector<int>(n, 0));
   for(int i = 0; i < n; i++) {
     for(int j = 0; j < n; j++) {
@@ -27,8 +34,28 @@ Matriz multiplyStandar(const Matriz& a, const Matriz& b, int n) {
   }
   return c;
 }
-    
+
+void print(const Matriz& a) {
+  int n = a.size();
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < n; j++) {
+      cout << "Matriz[" << i << "][" << j <<"] = " << a[i][j] << endl;
+    }
+  }
+}
+
 int main(){
   srand(time(0));
-  
+  Matriz A = generateMatriz(2);
+  Matriz B = generateMatriz(2);
+  cout << "Matriz A:" << endl;
+  print(A);
+  cout << "Matriz B:" << endl;
+  print(B);
+
+  cout << "Matriz C:" << endl;
+  Matriz C = multiplyStandar(A, B);
+  print(C);
+
+  return 0;
 }
