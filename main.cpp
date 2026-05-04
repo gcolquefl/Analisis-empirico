db6bbc434c9b1ba825f1d3c7623af760e392cd32
gcolquefl <gcolquefl@unsa.edu.pe>
2026-05-03 22:28:32 -0500

    Implemente la función generateMatriz con valores aleatorios

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 typedef vector<vector<int>> Matriz;
+
+Matriz generateMatriz(int n) {
+  Matriz newMatriz(n, vector<int>(n, 0));
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < n; j++) {
+      newMatriz[i][j] = rand() % 10;
+    }
+  }
+  return newMatriz;
+}
 
 Matriz multiplyStandar(const Matriz& a, const Matriz& b, int n) {
   Matriz c(n, vector<int>(n, 0));
@@ -15,7 +27,8 @@ Matriz multiplyStandar(const Matriz& a, const Matriz& b, int n) {
   }
   return c;
 }
-
+    
 int main(){
+  srand(time(0));
   
 }
